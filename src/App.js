b45c8b6eedf3c6de67d01df2a4db35dbb183126d
Bilal Tüylü <btuylu@nuevoyazilim.com>
2020-05-01 17:53:51 +0300

    second-commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Navi from "./Navi";
+import CategoryList from "./CategoryList";
+import ProductList from "./ProductList";
 
 function App() {
   return (
     <div className="App">
-      <Navi></Navi>
+      <Navi />
+      <CategoryList />
+      <ProductList />
     </div>
   );
 }
